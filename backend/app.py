ead81f9914fc7b2de93742dfbfe7b1fbaeb07f95
Khubaib Ahmed <kahmed.bscs23seecs@seecs.edu.pk>
2025-05-05 20:58:11 +0500

    integrated into app.py

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from backend.routes.flight_routes import options_flight,flight
 from backend.routes.restaurant_api import place_photo,random_photo,restaurant
+from backend.routes.nearby_search import get_data
 from backend.chatbot.chatbot_chaining import chat,reset_conversation
 
 app = FastAPI()
@@ -24,6 +25,10 @@ app.add_api_route("/place-photo",endpoint=place_photo,methods = ["GET"])
 app.add_api_route("/random-photo",endpoint=random_photo,methods=["GET"])
 app.add_api_route("/restaurant",endpoint=restaurant,methods=["POST"])
 
+## NEARBY SEARCH
+app.add_api_route("/get-data",endpoint=get_data,methods=["POST","GET"])
+
 ## CHATBOT_CHAINING ROUTERS
 app.add_api_route("/chat",endpoint=chat,methods=["POST"])
 app.add_api_route("/reset",endpoint=reset_conversation,methods=["POST"])
+
