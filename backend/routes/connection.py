06449d4db98a8278ef573f91bfcafeaf669a876d
Khubaib Ahmed <kahmed.bscs23seecs@seecs.edu.pk>
2025-05-11 16:35:30 +0500

    used mongo and redis cloud

--- backend/routes/connection.py
+++ backend/routes/connection.py
@@ -1,22 +1,24 @@
 from pymongo import MongoClient
-import redis
+from upstash_redis import Redis
 
-# MongoDB connection with pooling
-mongo_client = MongoClient(
-    "mongodb://localhost:27017/",
-    maxPoolSize=100,
-    minPoolSize=10,
-    serverSelectionTimeoutMS=5000
-)
+
+from dotenv import load_dotenv
+import os
+
+# Load .env file
+load_dotenv()
+
+# Print MONGO_URL and REDIS_URL to ensure they are being loaded correctly
+MONGO_URL = os.getenv('MONGO_URL')
+REDIS_URL = os.getenv('REDIS_URL')
+# MongoDB connection
+mongo_client = MongoClient(MONGO_URL, maxPoolSize=100, minPoolSize=10, serverSelectionTimeoutMS=5000)
 mongo_db = mongo_client['tourism']
 
 # Redis connection with pooling
-redis_client = redis.Redis(
-    host='localhost',
-    port=6379,
-    db=0,
-    decode_responses=True,  # To get string responses
-    socket_connect_timeout=5,
-    socket_timeout=5,
-    max_connections=50
-)
+if not REDIS_URL:
+    raise ValueError("REDIS_URL is not set in your environment variables")
+
+redis_client =Redis(url="https://welcome-zebra-29045.upstash.io", token="AXF1AAIjcDE0OTFiNTVkNjE5MzY0NTVhYjExODg0MTY5YWZmMTg2ZnAxMA")
+
+
