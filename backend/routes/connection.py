390ac5b7ddaae231468d9002a0fa20f8acf73733
Khubaib Ahmed <kahmed.bscs23seecs@seecs.edu.pk>
2025-05-11 16:45:09 +0500

    added redis server

--- backend/routes/connection.py
+++ backend/routes/connection.py
@@ -11,6 +11,7 @@ load_dotenv()
 # Print MONGO_URL and REDIS_URL to ensure they are being loaded correctly
 MONGO_URL = os.getenv('MONGO_URL')
 REDIS_URL = os.getenv('REDIS_URL')
+REDIS_TOKEN = os.getenv('REDIS_TOKEN')
 # MongoDB connection
 mongo_client = MongoClient(MONGO_URL, maxPoolSize=100, minPoolSize=10, serverSelectionTimeoutMS=5000)
 mongo_db = mongo_client['tourism']
@@ -35,6 +36,6 @@ def ensure_indexes():
 if not REDIS_URL:
     raise ValueError("REDIS_URL is not set in your environment variables")
 
-redis_client =Redis(url="https://welcome-zebra-29045.upstash.io", token="AXF1AAIjcDE0OTFiNTVkNjE5MzY0NTVhYjExODg0MTY5YWZmMTg2ZnAxMA")
+redis_client =Redis(url=REDIS_URL, token=REDIS_TOKEN)
 
 
