bead283a8d54643aaede5c0bba0d0be2bae77366
moizgittpro <Abdulmoiz27j@gmail.com>
2025-06-22 15:57:59 +0500

    removing backend stuff for front end branch

--- backend/config/settings.py
+++
@@ -1,6 +0,0 @@
-import os
-
-RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-GOOGLE_PLACES_API_KEY = os.getenv("google_places_api_key ")
-# FLIGHTS_API_BASE_URL = "https://booking-com15.p.rapidapi.com/api/v1/flights"
-GOOGLE_PLACES_URL = "https://maps.googleapis.com/maps/api/place/textsearch/json"
